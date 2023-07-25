df00c7b31c925086b9eb8b9b509e7087975e076d
Vicente Antonio G. Reyes <icenreyes@icloud.com>
2023-07-25 18:52:26 +0800

    added README and finished code

---
+++ README.md
@@ -0,0 +1,12 @@
+## A desktop app that gets jokes from the icanhazdadjoke.com API
+
+
+### To run the app
+#### start a virtual environment
+`python -m venv venv`
+
+#### install dependencies
+`python -m pip install -r requirements.txt`
+
+#### Run app
+`python -m main`
--- main.py
+++ main.py
@@ -2,21 +2,24 @@ import requests
 import ttkbootstrap as tb
 from ttkbootstrap.constants import *
 
-root = tb.Window(themename='darkly')
-root.title('Joke App')
-root.geometry('1280x900')
-
-def get_joke():
+def get_joke(label_text):
     url = "https://icanhazdadjoke.com"
     headers = {'Accept': 'application/json'}
     joke_time = requests.get(url, headers=headers).json().get('joke')
-    label.config(text=joke_time)
+    label_text.set(joke_time)
     print(joke_time)
 
-label = tb.Label(text="", font=("Poppins", 16), bootstyle='default')
-label.pack(padx=5, pady=10)
-btn = tb.Button(text="Get Joke!", command=get_joke)
-btn.pack(padx=5, pady=10)
+def start(master) -> None:
+    label_text = tb.StringVar()
+    label = tb.Label(master, textvariable=label_text, font=("Poppins", 16), bootstyle='default')
+    label.pack(padx=5, pady=10)
+    btn = tb.Button(master, text="Get Joke!", command=lambda: get_joke(label_text))
+    btn.pack(padx=5, pady=10)
 
-
-root.mainloop()
+if __name__ == '__main__':
+    app  = tb.Window(themename='darkly')
+    app.title('Joke App')
+    app.geometry('1280x900')
+    start(app)
+    app.mainloop()
+    
